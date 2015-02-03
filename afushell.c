b069442c206d8d55cd3b9b116fbb62d9debaacc1
yikoudreams1 <yikoudreams@bupt.edu.cn>
2015-02-02 18:54:47 -0800

    version 3.0

--- afushell.c
+++ afushell.c
@@ -7,42 +7,56 @@
 
 
 int cmdtoargulist(char* cmdLine,char* argList[20]);
-
-int main(void){
+void printprompt();
+int buildincmd(char * arguments[]);
+int main(void)
+{
         char buf[MAXLINE];
         pid_t pid;
         int status;
         char* argulist[20];
         int i=0,j=0;
-        printf("%% ");
-        while(fgets(buf,MAXLINE,stdin)!=NULL){
-             if (buf[strlen(buf)-1] == '\n'){
+        printf("Welcome to afushell !\n ");
+        printprompt();
+        while(fgets(buf,MAXLINE,stdin)!=NULL)
+        {
+             if (buf[strlen(buf)-1] == '\n')
+             {
                  buf[strlen(buf)-1] = 0;
-               }   
+            }   
 
-       for(i=0;i<20;i++)
-       {
-           argulist[i]=(char*)malloc(sizeof(char)*100);
-           memset(argulist[i],'\0',sizeof(char)*100);
-                   }
+            for(i=0;i<20;i++)
+            {
+                argulist[i]=(char*)malloc(sizeof(char)*100);
+                memset(argulist[i],'\0',sizeof(char)*100);
+           }
              
-       if(cmdtoargulist(buf,argulist)==1)
-       {printf("explan the cmd is wrong");}
+             if(cmdtoargulist(buf,argulist)==1)
+            {
+              printf("explan the cmd is wrong");
+            }
 
-             if((pid = fork())<0){
-               printf("fork error");
-               }else if(pid == 0){ 
+             if(buildincmd(argulist))
+             {
+                 if((pid = fork())<0)
+                 {
+                    printf("fork error");
+                }
+                 else if(pid == 0)
+                 { 
                       printf("%s",buf);
                       execvp(argulist[0],argulist);
-                      printf("couldn't execute: %s",buf);
+                      printf("couldn't execute: %s\n",buf);
                       exit(127);
                   }   
 
                                                     /*parent*/
-        if ((pid=waitpid(pid,&status,0))<0){
-            printf("waitpid error");
-            }   
-         printf("%% ");
+                 if ((pid=waitpid(pid,&status,0))<0)
+                 {
+                    printf("waitpid error");
+                }
+             }
+         printprompt();
              }   
           exit(0);
 }
@@ -77,4 +91,87 @@ int cmdtoargulist(char* cmdLine,char* argList[20])
              }
         }
 
+}
+void printprompt()
+{
+    char *promptenvironment=getenv("PE");
+         char buf[MAXLINE];
+    if(promptenvironment==NULL)
+    {
+
+                         printf("%s@",getlogin());
+                        gethostname(buf,MAXLINE);
+                        printf("%s:",buf);
+                         getcwd(buf,MAXLINE);
+                         printf("%s>",buf);
+    }
+    else 
+    {
+         int len=strlen(promptenvironment);
+         int i;
+         for(i=0;i<len;i++)
+         {
+             if(promptenvironment[i]='\\')
+             {
+                 switch(promptenvironment[++i])
+                 {
+                     case 'u':
+                         printf("%s",getlogin());
+                         break;
+                     case 'h':
+                         getcwd(buf,MAXLINE);
+                         printf("%s",buf);
+                         break;
+                    case 'w':
+                        gethostname(buf,MAXLINE);
+                        printf("%s",buf);
+                        break;
+                    default:
+                        printf("%c",promptenvironment[i]);
+                }
+             }
+             printf("%c",promptenvironment[i]);
+         }
+    }         if(strcmp("root",getlogin())==0)
+             printf(" # ");
+         else printf(" $ ");
+    
+    fflush(NULL);
+}
+
+
+int buildincmd(char * arguments[])
+{
+    if(strcmp(arguments[0],"cd")==0)
+    {
+            if(chdir(arguments[1])!=0)
+                    printf("change directory failed\n");
+            return 0;
+    }
+    else if(strcmp(arguments[0],"exit")==0)
+    {        printf("Thank you for use afushell,bye!\n");
+            exit(0);       
+    }    
+    else if(strcmp(arguments[0],"export")==0)
+    {        char *name,*value;
+            name=strtok(arguments[1],"=");
+            value=strtok(NULL,"+");
+            if(name==NULL || value==NULL)
+            {
+                printf("set environment failed\n");
+                return 0;
+            }
+            if(setenv(name,value,1)!=0)
+            {
+                printf("set environment failed\n");
+                return 0;
+            }
+    }
+    else if(strcmp(arguments[0],"echo")==0)
+    {        printf("%s\n",getenv(arguments[1]));
+            return 0;
+    }
+    else    return 1;
+    
+    
 }
